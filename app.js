1abbe7495ace4e856b7191feea2cd1545aea8d8c
Oreofeoluwapo Olutola <oreofeoluwapo.olutola@andela.com>
2017-12-14 11:28:51 +0100

    Add morgan to log incoming requests

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 var db = require('./db');
+var morgan = require('morgan');
+
+// logging user requests
+app.use(morgan('dev'));
 
 var UserController = require('./user/UserController');
 app.use('/users', UserController);
